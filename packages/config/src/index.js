31c3bfc0a468d061c6ffdc05d8e826df6163bf61
Ivan Burnaev <ivan.biirus@gmail.com>
2019-03-01 14:36:09 +0300

    feat(env): add env configs to @yva/config
    affects: @yva/config
    
    To get runtime config I add getEnv function to @yva/config

--- packages/config/src/index.js
+++ packages/config/src/index.js
@@ -1,3 +1,13 @@
 export const getConfig = () => {
   return !!window && !!window.config ? window.config : {};
 };
+
+export const getEnv = key => {
+  const env = process.env.NODE_ENV === "development" ? process.env : window.env;
+
+  if (key) {
+    return env[key.includes("REACT_APP_") ? key : `REACT_APP_${key}`];
+  }
+
+  return env || {};
+};
--- packages/config/test/index.test.js
+++ packages/config/test/index.test.js
@@ -1,4 +1,4 @@
-import { getConfig } from "../src/index";
+import { getConfig, getEnv } from "../src/index";
 
 describe("getConfig", () => {
   it("should retun an empty object if is no config has been found", () => {
@@ -16,5 +16,68 @@ describe("getConfig", () => {
     expect(getConfig()).toEqual({
       SSO: "http://localhost/sso",
     });
+  });
+});
+
+describe("getEnv", () => {
+  beforeEach(() => {
+    global.process = { env: {} };
+  });
+
+  it("should retun an empty object if is no env has been found", () => {
+    const env = getEnv();
+
+    expect(typeof env).toEqual("object");
+    expect(Object.keys(env).length).toEqual(0);
+  });
+
+  it("should return process.env in development mode", () => {
+    global.process = {
+      env: {
+        NODE_ENV: "development",
+      },
+    };
+
+    expect(getEnv()).toEqual({
+      NODE_ENV: "development",
+    });
+  });
+
+  it("should return window.env in production mode", () => {
+    global.process = {
+      env: {
+        NODE_ENV: "production",
+      },
+    };
+
+    window.env = {
+      API_PATH: "/api",
+    };
+
+    expect(getEnv()).toEqual({
+      API_PATH: "/api",
+    });
+  });
+
+  it("should return env key", () => {
+    global.process = {
+      env: {
+        NODE_ENV: "development",
+        REACT_APP_API_ROOT: "/api-root",
+      },
+    };
+
+    expect(getEnv("REACT_APP_API_ROOT")).toEqual("/api-root");
+  });
+
+  it("should append REACT_APP to env key if it has not", () => {
+    global.process = {
+      env: {
+        NODE_ENV: "development",
+        REACT_APP_API_ROOT: "/api-root",
+      },
+    };
+
+    expect(getEnv("API_ROOT")).toEqual("/api-root");
   });
 });
