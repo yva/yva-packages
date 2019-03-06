2264cf5cd1e483815ff04bd7b9af8531d28711de
Ivan Burnaev <ivan.biirus@gmail.com>
2019-03-06 15:57:35 +0300

    fix(session): fix 401 error handling. There was wrong config usage
    affects: @yva/session
    
    To handle 401 error we need to redirect user to the SSO and to get him back we have to pass
    redirectUri. So it had been made wrong way by using old \`getConfig\` method

--- packages/session/src/epics/handle-401.js
+++ packages/session/src/epics/handle-401.js
@@ -1,7 +1,7 @@
 import { filter, map } from "rxjs/operators";
 import { isErrorAction } from "redux-async-epic";
 import { replace } from "react-router-redux";
-import { getConfig } from "@yva/config";
+import { getEnv } from "@yva/config";
 
 const is401 = ({ payload }) => {
   return payload && payload.status === 401;
@@ -11,10 +11,12 @@ export const handle401 = action$ =>
   action$.pipe(
     filter(action => isErrorAction(action) && is401(action)),
     map(() => {
+      const basename = getEnv("ROOT");
+
       return replace({
         pathname: "/auth/refresh",
         state: {
-          pathname: window.location.pathname.replace(getConfig().BASE_NAME, ""),
+          pathname: window.location.pathname.replace(basename, ""),
         },
       });
     })
--- packages/session/test/epics/handle-401.test.js
+++ packages/session/test/epics/handle-401.test.js
@@ -2,6 +2,17 @@ import { of } from "rxjs";
 import { error } from "redux-async-epic";
 import { handle401 } from "../../src/epics/handle-401";
 
+beforeAll(() => {
+  window.env = {
+    REACT_APP_ROOT: "/dashboard",
+    REACT_APP_API: "http://localhost/api",
+    REACT_APP_SSO: "http://localhost/sso",
+    REACT_APP_CLIENT_ID: "5a9602da0f2ce70fd438bc43",
+    REACT_APP_REDIRECT_URI: "/auth",
+    REACT_APP_VERSION: "1.0.1",
+  };
+});
+
 describe("handle401", () => {
   it("should call `replace` action when 401 error is come", done => {
     const action$ = of({
@@ -23,7 +34,6 @@ describe("handle401", () => {
           args: [{ pathname: "/auth/refresh", state: { pathname: "/" } }],
           method: "replace",
         });
-
         done();
       },
     });
